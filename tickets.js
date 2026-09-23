ef9835a08e5dc2647e0daa2c284e406cc21695b4
Elton Jay Cimafranca <yourgithubemail@example.com>
2026-09-23 20:46:54 +0800

    A Add flat 10 dollar order discount - cimafranca.eltonjay
    :wq

--- tickets.js
+++ tickets.js
@@ -2,12 +2,19 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  let price = basePrice * quantity;
+function calculateTicketPrice(quantity, basePrice, isVIP) {
+  let price = quantity * basePrice;
 
-  if(quantity >= 5) {
-    price *= 0.9; 
+  if (quantity >= 5) {
+    price *= 0.9;
   }
+
+  if (isVIP) {
+    price *= 1.5;
+  }
+
+  price -= 10;
+
   return Math.round(price);
 }
 
